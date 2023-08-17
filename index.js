a8d6a1585f88cfa1b63d532a7d96c94fa1cc097d
rabiulahsan <ra.rabiulahsan@gmail.com>
2023-08-17 20:44:48 +0600

    api for get all repositories

--- index.js
+++ index.js
@@ -44,7 +44,11 @@ async function run() {
     res.send(result);
   });
 
- 
+  //get all repositories
+  app.get('/repositories', async(req, res)=>{
+    const result = await repositoriesCollection.find().toArray();
+      res.send(result);
+})
 
    // Send a ping to confirm a successful connection
    await client.db("admin").command({ ping: 1 });
