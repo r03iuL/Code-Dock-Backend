2f37059d68477e8affb9f255afe324dafe4e2592
istiakantu <istakahmed636@gmail.com>
2023-09-06 22:29:56 +0600

    chat code try

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require('express');
+
+const bodyParser = require('body-parser');
+
+
 const app = express();
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
+const mongoose = require('mongoose');
 
 
 // middleware
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.json());
 
 
 
@@ -34,6 +40,7 @@ async function run() {
     //database collection
     const usersCollection = client.db("code-dock").collection("users");
     const repositoriesCollection = client.db("code-dock").collection("repositories");
+    const messages = client.db("code-dock").collection("message");
 
     // Saved user API
 
@@ -55,7 +62,7 @@ async function run() {
       res.send(result);
     })
 
-    
+
 
     //create a new repository
     app.post("/new", async (req, res) => {
@@ -90,7 +97,48 @@ async function run() {
 
       const result = await repositoriesCollection.insertOne(repoDetails);
       res.status(201).json({ message: 'Repository created successfully', repo: result });
-  });
+    });
+
+
+
+    // Live chat apis (New Feature)
+
+    // const Message = mongoose.model('Message', { text: String, sender: String, timestamp: Date });
+
+    // app.post('/user/sendMessage', async (req, res) => {
+    //   const { text } = req.body;
+    //   const newMessage = new Message({ text, sender: 'admin', timestamp: new Date() });
+
+    //   try {
+    //     await newMessage.save();
+
+    //     // Simulate a delay before sending the auto-reply
+    //     setTimeout(async () => {
+    //       const autoReply = new Message({
+    //         text: 'Thank you for your message! An admin will get back to you shortly.',
+    //         sender: 'admin',
+    //         timestamp: new Date(),
+    //       });
+    //       await autoReply.save();
+
+    //       res.status(200).json({ message: 'Message sent successfully.' });
+    //     }, 1000); // Adjust the delay as needed
+    //   } catch (error) {
+    //     res.status(500).json({ error: 'An error occurred while sending the message.' });
+    //   }
+    // });
+
+    // app.get('/user/getMessages', async (req, res) => {
+    //   try {
+    //     const messages = await Message.find();
+    //     res.status(200).json({ messages });
+    //   } catch (error) {
+    //     res.status(500).json({ error: 'An error occurred while fetching messages.' });
+    //   }
+    // });
+
+
+
 
 
 
