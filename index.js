4efc203b6a0f86c30ef15e08e8bdf29bfa762b02
tisha-akter <tishaakter.jui79@gmail.com>
2023-09-08 17:54:34 +0600

    fixed backend issue

--- index.js
+++ index.js
@@ -12,6 +12,24 @@ app.use(cors());
 app.use(express.json());
 
 
+// JWT verify
+
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorized users' })
+  }
+
+  const token = authorization.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: true, message: 'unauthorized users' })
+    }
+    req.decoded = decoded;
+    next()
+  })
+}
+
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -41,6 +59,13 @@ async function run() {
     console.log("snippetsCollection created:", snippetsCollection.collectionName);
 
 
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+      res.send({ token })
+    })
+
+
 
     // Create a new code snippet
     app.post("/snippets", async (req, res) => {
@@ -57,13 +82,6 @@ async function run() {
       res.send(result);
     })
 
-    // Get a single code snippet by _id
-    // app.get('/snippets/:id', async (req, res) => {
-    //   const id = req.params.id;
-    //   const query = { _id: new ObjectId(id) }
-    //   const result = await snippetsCollection.findOne(query);
-    //   res.send(result);
-    // });
 
 
     app.get('/snippets/:id', async (req, res) => {
@@ -89,7 +107,25 @@ async function run() {
     });
 
 
+    // Saved user API
 
+    app.get('/users', async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result)
+    })
+
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email }
+      const existingUser = await usersCollection.findOne(query)
+      if (existingUser) {
+        return res.send({ message: 'user already exists' })
+      }
+
+      user.role = "user"
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    })
 
 
 
@@ -103,7 +139,7 @@ async function run() {
     });
 
     //get all repositories
-    app.get('/repositories', async (req, res) => {
+    app.get('/repositories', verifyJWT, async (req, res) => {
       const result = await repositoriesCollection.find().toArray();
       res.send(result);
     })
